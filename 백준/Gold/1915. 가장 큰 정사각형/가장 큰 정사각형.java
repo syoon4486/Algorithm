fb4a94c4be51eba96c10675cbb84f586100c7b18
Daeun Yoon <112847006+syoon4486@users.noreply.github.com>
2024-09-25 11:17:03 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 200 ms, Memory: 21284 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -10,19 +10,18 @@ public class Main{
 		int n = Integer.parseInt(s[0]);
 		int m = Integer.parseInt(s[1]);
 		
-		String st;		
-		int max = 0;
-		int[][] arr = new int[n][m];
-		for (int i = 0; i < n; i++) {
+		String st;
+		int[][] arr = new int[n+1][m+1];
+		for (int i = 1; i <= n; i++) {
 			st = br.readLine();
-			for (int j = 0; j < m; j++) {
-				arr[i][j] = st.charAt(j)-'0';
-                if (arr[i][j] == 1) max = 1;
+			for (int j = 1; j <= m; j++) {
+				arr[i][j] = st.charAt(j-1)-'0';
 			}
 		}
-
-		for (int i = 1; i < n; i++) {
-			for (int j = 1; j < m; j++) {
+		
+		int max = 0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
 				if (arr[i][j] == 1) {
 					arr[i][j] = Math.min(arr[i-1][j], Math.min(arr[i][j-1], arr[i-1][j-1])) + 1;
 					max = Math.max(arr[i][j], max);
