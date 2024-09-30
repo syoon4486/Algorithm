e0cd67f6930de652e57446a4522ab06ef12f2fdf
Daeun Yoon <112847006+syoon4486@users.noreply.github.com>
2024-09-30 15:32:56 +0900

    [Gold V] Title: 토마토, Time: 652 ms, Memory: 149548 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] s = br.readLine().split(" ");
+		int M = Integer.parseInt(s[0]);
+		int N = Integer.parseInt(s[1]);
+		
+		int[][] tomato = new int[N][M];
+		Queue<int[]> queue = new LinkedList<>();
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			s = br.readLine().split(" ");
+			for (int j = 0; j < M; j++) {
+				tomato[i][j] = Integer.parseInt(s[j]);
+				if (tomato[i][j] == 1) {
+					queue.offer(new int[] {i, j, 0});
+				} else if (tomato[i][j] == 0) cnt++;
+			}
+		}
+		if (queue.isEmpty()) {
+			System.out.println(cnt == 0 ? 0 : -1);
+			return;
+		}
+		
+		int[][] d = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+		while (true) {
+			int[] tmp = queue.poll();
+			
+			for (int i = 0; i < 4; i++) {
+				int nr = tmp[0] + d[i][0];
+				int nc = tmp[1] + d[i][1];
+				if (nr < 0 || nr >= N || nc < 0 || nc >= M || tomato[nr][nc] != 0) continue;
+				tomato[nr][nc] = 1;
+				cnt--;
+				queue.offer(new int[] {nr, nc, tmp[2]+1});
+			}
+			
+			if (queue.isEmpty()) {
+				System.out.println(cnt == 0 ? tmp[2] : -1);
+				return;
+			}
+		}
+	}
+}
